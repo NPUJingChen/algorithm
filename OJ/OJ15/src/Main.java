907a93c94079fc3bea9252d5d56cc52ef9f9d018
NPUJingChen <1204469551@qq.com>
2022-11-24 23:38:39 +0800

    2022/11/24 15题

--- OJ/OJ15/src/Main.java
+++ OJ/OJ15/src/Main.java
@@ -1,5 +1,12 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+import java.util.*;
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        System.out.println(str);
     }
 }
