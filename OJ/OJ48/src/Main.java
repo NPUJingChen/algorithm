92c5069e4c7b3ab795e3e15f155081667b34aa78
NPUJingChen <1204469551@qq.com>
2022-12-01 19:49:05 +0800

    48

--- OJ/OJ48/src/Main.java
+++ OJ/OJ48/src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
+    public static int maxProduct(int[] nums) {
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+        System.out.println(maxProduct(nums));
     }
 }
