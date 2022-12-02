72432d3a1b5b2afb9d9dabd601c079932f416bdc
NPUJingChen <1204469551@qq.com>
2022-12-02 22:39:10 +0800

    add 61

---
+++ OJ/OJ61/src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+/**
+ * @author chenjing
+ */
+public class Main {
+    public static int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int[] dp = new int[n];
+        dp[0] = 1;
+        int max = 1;
+        for (int i = 1; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+        System.out.println(lengthOfLIS(nums));
+    }
+}
