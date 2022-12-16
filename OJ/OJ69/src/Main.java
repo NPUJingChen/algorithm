1c3a92789fc84a169272b5f715e7efac3536e5a5
NPUJingChen <1204469551@qq.com>
2022-12-16 14:09:51 +0800

    add 69

---
+++ OJ/OJ69/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+/**
+ * @author chenjing
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+        System.out.println(wiggleMaxLength(nums));
+    }
+
+    public static int wiggleMaxLength(int[] nums) {
+        int n = nums.length;
+        if (n < 2) {
+            return n;
+        }
+        int prevdiff = nums[1] - nums[0];
+        int ret = prevdiff != 0 ? 2 : 1;
+        for (int i = 2; i < n; i++) {
+            int diff = nums[i] - nums[i - 1];
+            if ((diff > 0 && prevdiff <= 0) || (diff < 0 && prevdiff >= 0)) {
+                ret++;
+                prevdiff = diff;
+            }
+        }
+        return ret;
+    }
+}
